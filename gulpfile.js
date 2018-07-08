b7bba58ad97332b3e3446e2ec5c9b6744319abb7
darya_kochubey <kochubeyalex71@gmail.com>
2018-07-08 11:41:20 +0200

    Gulp configurated

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
 var browserSync = require('browser-sync').create();
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+
+gulp.task('postcss', ['sass'], function () {
+    var plugins = [
+        autoprefixer({browsers: ['last 15 version']}),
+        cssnano()
+    ];
+    return gulp.src('project/css/*.css')
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('project/css'))
+        .pipe(browserSync.reload({
+            stream: true
+          }));
+});
 
 const pathToSass = 'project/sass/*.scss'
 
@@ -8,9 +24,6 @@ gulp.task ('sass', function () {
     return gulp.src(pathToSass)
         .pipe(sass())
         .pipe(gulp.dest('project/css'))
-        .pipe(browserSync.reload({
-            stream: true
-          }))
 });
 
 gulp.task('reload', function() {
@@ -21,8 +34,9 @@ gulp.task('reload', function() {
     })
 })
 
-gulp.task('watch' , function() {
-    gulp.watch(pathToSass, ['sass']);
+gulp.task('watch', ['sass', 'postcss', 'reload'], function() {
+    gulp.watch(pathToSass, ['sass', 'postcss']);
+    gulp.watch('project/index.html', browserSync.reload)
 });
 
 gulp.task('default', ['watch']);
